182be5d49d60bf85f2803187e268d7cb656e8ffb
Sahil <sahillal25@outlook.de>
2023-06-14 22:44:55 +0200

    Fehler bezüglich server behoben

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async(e) =>{
   
   loader(messageDiv);
   const input = name + " " + data.get('promt');
-  const response = await fetch('http://localhost:5000/',{
+  const response = await fetch('https://chat-bot-theta-one.vercel.app/',{
     method: 'POST',
     headers :{
       'Content-Type': 'application/json'
